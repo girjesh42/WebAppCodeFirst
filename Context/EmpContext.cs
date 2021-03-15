e501ed5fb80e8d6b7097f49cb229aabbf2790587
girjesh42 <kumargirjesh2442@gmail.com>
2021-03-15 15:48:16 +0530

    Second commit

---
+++ Context/EmpContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebAppCodeFirst.Models;
+
+namespace WebAppCodeFirst.Context
+{
+    public class EmpContext:DbContext
+    {
+        public DbSet<Emp> Emps { get; set; }
+           
+    }
+}
---
+++ Models/Emp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppCodeFirst.Models
+{
+    public class Emp
+    {
+        public int EmpId { get; set; }
+        public string Ename { get; set; }
+        public string Edesig { get; set; }
+        public DateTime Edoj { get; set; }
+    }
+}
